c35885a7147df53b97b5b8789f73d38dfe6de5ec
Sandy Ricardo Acevedo Peralta <51434989+psyk4e@users.noreply.github.com>
2025-01-31 17:19:34 -0400

    fix: remove legacy HTTPS agent configuration to prevent SSL handshake failures

--- src/networking/restClient.ts
+++ src/networking/restClient.ts
@@ -16,13 +16,13 @@ export enum BaseUrl {
 
 export const restClient = axios.create({
   baseURL: BaseUrl.ECF,
-  httpsAgent: new https.Agent({
-    // node 18x
-    // https://stackoverflow.com/questions/74324019/allow-legacy-renegotiation-for-nodejs/74600467#74600467
-    // rejectUnauthorized: false,
-    // allow legacy server
-    secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
-  }),
+  // httpsAgent: new https.Agent({
+  //   // node 18x
+  //   // https://stackoverflow.com/questions/74324019/allow-legacy-renegotiation-for-nodejs/74600467#74600467
+  //   // rejectUnauthorized: false,
+  //   // allow legacy server
+  //   secureOptions: crypto.constants.SSL_OP_LEGACY_SERVER_CONNECT,
+  // }), //? this is making the request to fail with the error: "Error: write EPROTO 140735259084352:error:14094410:SSL routines:ssl3_read_bytes:sslv3 alert handshake failure:../deps/openssl/openssl/ssl/record/rec_layer_s3.c:1407:SSL alert number 40"
 });
 
 restClient.interceptors.response.use(
